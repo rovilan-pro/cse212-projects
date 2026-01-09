153df3bd6d0638f9cc7f599abe8a899368cd1bce
rovilan-pro <rovilec143@gmail.com>
2026-01-09 12:39:15 +0800

    Array and Divisor

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -10,9 +10,9 @@ ArraySelector.Run();
 
 
 
-// Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nDivisors Solution\n======================");
-// DivisorsSolution.Run();
-//
-// Console.WriteLine("\n======================\nArray Selector Solution\n======================");
-// ArraySelectorSolution.Run();
+
+Console.WriteLine("\n======================\nDivisors Solution\n======================");
+DivisorsSolution.Run();
+
+Console.WriteLine("\n======================\nArray Selector Solution\n======================");
+ArraySelectorSolution.Run();
